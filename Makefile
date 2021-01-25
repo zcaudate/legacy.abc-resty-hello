8b2591bdecad0b95b652f4f9738392b876ad0868
Chris Zheng <z@caudate.me>
2021-01-26 01:24:22 +0800

    stuff

--- Makefile
+++ Makefile
@@ -1,6 +1,8 @@
 dev:
-	echo src/response.lua | entr -r nginx -s reload
+	echo src/response.lua | entr -r echo 'RELOAD' && nginx -s reload
+
 start:
 	nginx -p runtime -c ../nginx.conf
+
 stop:
 	nginx -s stop
--- src/response.lua
+++ src/response.lua
@@ -1,18 +1,19 @@
-js = require('cjson')
+local js = require('cjson')
+local n = require('ngx')
 
-function test_response()
-  ngx.status = ngx.HTTP_OK
-  ngx.say(js.encode({status=true,foo='bar'}))
-  return ngx.exit(ngx.HTTP_OK)
+local function test_response()
+  n.status = ngx.HTTP_OK
+  n.say(js.encode({status=true,foo='bar'}))
+  return n.exit(n.HTTP_OK)
 end
 
-function echo_reponse()
-  local get, post, files = require('resty.reqargs')
-  ngx.status = ngx.HTTP_OK
-  ngx.say(js.encode(post))
-  return ngx.exit(ngx.HTTP_OK)
+local function echo_reponse()
+  n.log('339989b2-8f7e-437c-9c22-d6582d648c7d')
+  n.status = n.HTTP_OK
+  n.say(js.encode({status=true,value='PONG'}))
+  return n.exit(n.HTTP_OK)
 end
 
-MODULE = {test_response=test_response,echo_response=echo_reponse}
+local MODULE = {test_response=test_response,echo_response=echo_reponse}
 
 return MODULE
